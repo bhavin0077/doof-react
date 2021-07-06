9787336247dd607c679a521b27ba0659b54be2c0
bhavin bhagat <bhavinsbhagat07@gmail.com>
2021-07-06 12:49:14 +0530

    xhfgh

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+
+function App() {
+  return <div className="App"> 
+    
+    <div className="Background"></div>
+
+    <div className="YellowBubble"></div>
+    <div className="test">
+    Test
+    </div>
+    <div className="Bubble1"></div>
+    <div className="Bubble2"></div>
+
+    <div className="top">
+      Unlock your stuck revenue.
+    </div>
+
+    <div className="bottom">
+      we help you Unlock the true potential connect with your customers to
+      understan the rights and wrongs with your product.
+    </div>
+
+      <div className="title">
+        DOOF
+      </div>
+
+      <form className="LoginForm">
+      <div className ="login">Login
+         
+        <input className="email"
+          type="text"
+          placeholder="Email"
+        />
+ 
+
+        <input className="password"
+          type="text"
+          placeholder="Password"
+        />
+
+        
+
+        <button className="loginButton">Login</button>
+         </div>
+        <div className="signup"> Don't have an acoount yet? Signup</div>
+      </form>
+
+     <div className="terms">Terms of Service</div>
+     <div className="policy">Privacy Policy</div> 
+
+  </div>;
+}
+
+
+export default App;
